f2314add498c7bdf36ab4ceb1713bfa6cbd22a32
eroironico <nicolopano.np@gmail.com>
2023-06-16 10:49:22 +0200

    chore: attribute type

--- src/types/selector.ts
+++ src/types/selector.ts
@@ -1,7 +1,10 @@
+import { Attribute } from "./attribute"
 import Matcher from "./matcher"
 
 /**
  * if value is a string it acts as the `[attribute]` selector meaning that it will target all elements that just have that attribute
  */
-type Selector = string | [attribute: string, matcher: Matcher, value: string]
+type Selector =
+  | Attribute
+  | [attribute: Attribute, matcher: Matcher, value: string]
 export default Selector
